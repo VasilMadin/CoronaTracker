36390d8873077e183423b79dd129812cc963771f
Vasil Madin <51945879+VasilMadin@users.noreply.github.com>
2020-04-07 18:21:42 +0300

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+const url = "https://covid19.mathdro.id/api";
+
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(changeableUrl);
+
+    const modifiedData = {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
+    };
+
+    return modifiedData;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    return data.map(({ confirmed, deaths, reportDate: date }) => ({
+      confirmed: confirmed.total,
+      deaths: deaths.total,
+      date,
+    }));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (err) {
+    console.log(err);
+  }
+};
